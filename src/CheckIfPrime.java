64e8c5436100d207f49bf09fc5fa5021d38e955a
Robert van den Heuvel <robert_vd_heuvel@hotmail.com>
2019-04-01 09:17:11 +0200

    version 3

--- src/CheckIfPrime.java
+++ src/CheckIfPrime.java
@@ -2,13 +2,8 @@
 public class CheckIfPrime {
 	boolean checkIfPrime(long input){
 		boolean prime = true;
-		long divisor = input - 1;
-		if (input % 2 == 0) {
-			prime = false;
-			return prime;
-		}
-		else {
-			while(divisor>1) {
+		long divisor = 2;
+			while(divisor<input) {
 //				System.out.println("input = " + input);
 //				System.out.println("divisor = " + divisor);
 				if(input % divisor == 0) {
@@ -16,12 +11,11 @@ public class CheckIfPrime {
 //					System.out.println(input + " is not prime");
 					break;
 				}
-				divisor --;
+				divisor ++;
 			}
 			if(prime == true)
 				System.out.println(input);
 			return prime;
-		}
 
 	}
 }
